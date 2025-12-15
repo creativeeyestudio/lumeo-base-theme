e3a7cbe305e7ffe3d87bed5236899710ba41f2f9
creativeeyestudio <contact@creative-eye.fr>
2025-12-15 22:14:55 +0000

    Page and Shadcn config

---
+++ src/layouts/Page/Page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { PageProps } from './Page.interface'
+
+const Page: React.FC<PageProps> = (props) => {
+  return (
+    <div>Page</div>
+  )
+}
+
+export default Page
