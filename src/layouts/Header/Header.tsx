8a4cac0408c6d92a61ee6249c7e6a438b99303f1
creativeeyestudio <contact@creative-eye.fr>
2025-12-19 16:17:23 +0100

    Header CSS

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css'
 
 const Header: React.FC<HeaderProps> = () => {
   return (
-    <header></header>
+    <header className={styles.Header}></header>
   )
 }
 
